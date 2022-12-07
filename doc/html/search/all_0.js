c5e66319078b3d78f3f3eca1af5956807867bb2f
alenapp <alenapp692@gmail.com>
2022-12-07 22:43:27 +0100

    lab08

---
+++ doc/html/search/all_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['lab05_0',['lab05',['../lib_8c.html#a686538c4ccd24dbbc679c95398f2c1df',1,'lab05(a, b):&#160;lib.c'],['../lib_8h.html#aafcd5dd45cd6d750a1009ea26d6413b2',1,'lab05(int a, int b):&#160;lib.h']]],
+  ['lab06_1',['lab06',['../lib_8c.html#a318bdf1f140c210e9a525cba82ccbded',1,'lab06(k):&#160;lib.c'],['../lib_8h.html#a0d23095e4b10346cac5e0cda74351196',1,'lab06(long int k):&#160;lib.h']]],
+  ['lib_2ec_2',['lib.c',['../lib_8c.html',1,'']]],
+  ['lib_2eh_3',['lib.h',['../lib_8h.html',1,'']]]
+];
---
+++ doc/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['lab05_9',['lab05',['../lib_8c.html#a686538c4ccd24dbbc679c95398f2c1df',1,'lab05(a, b):&#160;lib.c'],['../lib_8h.html#aafcd5dd45cd6d750a1009ea26d6413b2',1,'lab05(int a, int b):&#160;lib.h']]],
+  ['lab06_10',['lab06',['../lib_8c.html#a318bdf1f140c210e9a525cba82ccbded',1,'lab06(k):&#160;lib.c'],['../lib_8h.html#a0d23095e4b10346cac5e0cda74351196',1,'lab06(long int k):&#160;lib.h']]]
+];
